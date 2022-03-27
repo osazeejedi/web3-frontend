7353e2815eb0407767abea25926b1d1b58225507
osazeejedi <oghagbonosazee20@gmail.com>
2022-03-27 10:45:44 +0100

    updated  Mystake

--- src/components/MyStake/MyStake.js
+++ src/components/MyStake/MyStake.js
@@ -4,61 +4,102 @@ import Card from './Card/Card'
 import Styles from './MyStake.module.css'
 
 const MyStake = ({
-  withdrawInput, 
-  stakeInput, 
-  onChangeInput, 
-  onClickStake, 
+  withdrawInput,
+  stakeInput,
+  onChangeInput,
+  onClickStake,
   onClickWithdraw,
   rewardAmount,
   stakeAmount,
-  connected
+  connected,
+  stakeDetails,
+  addressInput,
+  onClickAddress,
 }) => {
+  const { amount, address, time } = stakeDetails;
   return (
     <div className={Styles.root}>
-        <h2 className={Styles.heading}>My stake</h2>
+      <h2 className={Styles.heading}>My stake</h2>
 
-        <div className={Styles.stake_body}>
-          <div className={Styles.card_container}>
-            <Card 
-              cardKey="Total Staked"
-              cardValue = {stakeAmount}
-            />
-            <Card 
-              cardKey="Total Reward"
-              cardValue = {rewardAmount}
-            />
-          </div>
-          <form onSubmit={onClickStake} className={Styles.form} >
-            <input 
-              type = "number" 
-              placeholder="Amount to stake" 
-              className={Styles.input}
-              value = {stakeInput}
-              onChange = {onChangeInput}
-              id = "stake"
-            />
-            <button type='submit' className={clsx({[Styles.stake_btn]: true, [Styles.btn_diabled]: !connected})}
-              disabled = {!connected}
-            >Stake</button>
-          </form>
-
-          <form onSubmit = {onClickWithdraw} className={Styles.form} >
-            <input 
-              type = "number" 
-              placeholder="Amount to unstake" 
-              className={Styles.input}
-              value = {withdrawInput}
-              onChange = {onChangeInput}
-              id = "unstake"
-            />
-            <button type="submit"
-            className={clsx({[Styles.unstake_btn]: true, [Styles.btn_diabled]: !connected})}
-            disabled = {!connected}
-            >Unstake</button>
-          </form>
+      <div className={Styles.stake_body}>
+        <div className={Styles.card_container}>
+          <Card cardKey="Total Staked" cardValue={stakeAmount} />
+          <Card cardKey="Total Reward" cardValue={rewardAmount} />
         </div>
+        <form onSubmit={onClickStake} className={Styles.form}>
+          <input
+            type="number"
+            placeholder="Amount to stake"
+            className={Styles.input}
+            value={stakeInput}
+            onChange={onChangeInput}
+            id="stake"
+          />
+          <button
+            type="submit"
+            className={clsx({
+              [Styles.stake_btn]: true,
+              [Styles.btn_diabled]: !connected,
+            })}
+            disabled={!connected}
+          >
+            Stake
+          </button>
+        </form>
+
+        <form onSubmit={onClickWithdraw} className={Styles.form}>
+          <input
+            type="number"
+            placeholder="Amount to unstake"
+            className={Styles.input}
+            value={withdrawInput}
+            onChange={onChangeInput}
+            id="unstake"
+          />
+          <button
+            type="submit"
+            className={clsx({
+              [Styles.unstake_btn]: true,
+              [Styles.btn_diabled]: !connected,
+            })}
+            disabled={!connected}
+          >
+            Unstake
+          </button>
+        </form>
+        <form onSubmit={onClickAddress} className={Styles.form}>
+          <input
+            type="text"
+            placeholder="Search Detail of staker"
+            className={Styles.input}
+            value={addressInput}
+            onChange={onChangeInput}
+            id="address"
+          />
+          <button type="submit" className={clsx({ [Styles.stake_btn]: true })}>
+            Search
+          </button>
+        </form>
+      </div>
+      <div className={Styles.card}>
+        <h2 className={Styles.cc}>Stakers Information</h2>
+        <div className={Styles.cc}>
+          <h4>Address Staker</h4>
+          <p className={Styles.para}>{address}</p>
+        </div>
+        <div className={Styles.cc}>
+          <h4>Amount Staked</h4>
+          <p className={Styles.para}>
+            {amount} {`${amount ? `BRT` : 0.0}`}
+          </p>
+        </div>
+        <div className={Styles.cc}>
+          <h4>Time Staked</h4>
+          <p className={Styles.para}>{time}</p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default MyStake
